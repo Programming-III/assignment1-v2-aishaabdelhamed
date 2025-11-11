bdcf4c64b69c977e1315369361c90ce2f77bc5c2
aishaabdelhamed <aisha.abdelhamed@student.giu-uni.de>
2025-11-11 10:09:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,45 +4,71 @@
 #include "Student.h"
 #include "Instructor.h"
 #include "Course.h"
-
-
 using namespace std;
-
-// ==================== Person Class Implementation =========================
-
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
-    
-    return 0;
+class Person{
+      void Person(string name , int id){
+   this-> name=name;
+   this->id=id;
+   }
+   void display(){
+  cout<<"Name:"<<endl;
+  cout<<"id:"<<endl;
+  }
+};
+ 
+   class Student : Person{
+       void Student (int yearLevel , string major):Person(string name , int id){
+       this-> yearLevel=yearLevel;
+       this-> major=major;
+       }
+       void display(){
+       cout<<"yearLevel:"<<endl;
+       cout<<"major:"<<endl;
+       Student::display;
+       }
+   };
+   class Instructor : Person{
+    void Instructor(string department , int experienceYears):Person(string name , int id){
+    this-> department=department;
+    this-> experienceYears =experienceYears;
+    }
+    void display(){
+    cout<<"department:"<<endl;
+    cout<<"experienceYears:"<<endl;
+    Instructor:: display;
+    }
+   };
+  class Course{
+   void Course(string courseCode ,string coursename, int maxStudents, int currentStudents){
+       this-> courseCode=courseCode;
+       this-> coursename= coursename;
+       this-> maxStudents=maxStudents;
+       this-> currentStudents=currentStudents;
+       Student*student=nullptr;
+   }
+   void addStudent(const Student& s){
+       add += student;
+   }
+Student *student=new student{
+int count=n;
+for(int i;i<n;i++)
+   {
 }
+   void dispalyCourseInfo(){
+   cout<<"courseCode:"<<endl;
+   cout<<"coursename:"<<endl;
+   cout<<"maxStudent:"<<endl;
+   cout<<"currentStudent:"<<endl;
+   }
+  };
+  int main(){
+      
+      Student S1("Aisha","Cs","5","133004912");
+      S1.display();
+     Instructor I2("Dr.Lina","ComputerScience","4");
+      I2.display();
+      Course C3("Nabil", "3","programming");
+      C3.displayCourseInfo();
+      return 0;
+  }
+
