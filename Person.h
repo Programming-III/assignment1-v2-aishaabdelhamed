880c23a6794aaf3ae59670752ef1f9a490a0b75a
aishaabdelhamed <aisha.abdelhamed@student.giu-uni.de>
2025-11-11 10:00:07 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -1,10 +1,13 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include <string>
-using namespace std;
-
-//#write Person class here
+class Person{
+    private:
+    string name;
+    int id;
+    public:
+        void display();
+};
 
 
 
