8051ffa67d39c365f945a78fe7fd5329de39334d
aishaabdelhamed <aisha.abdelhamed@student.giu-uni.de>
2025-11-11 09:58:57 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,10 +1,20 @@
 #ifndef COURSE_H
 #define COURSE_H
 
-#include <string>
-#include "Student.h"
-using namespace std;
-//#write your code here
+class Course{
+    private:
+   string courseCode;
+   string coursename;
+   int maxStudents;
+   Student*student;
+   int currentStudents;
+    public:
+        void addStudent(const Student& s);
+        void displayCourseInfo();
+};
+
+
+#endif 
 
 
 
